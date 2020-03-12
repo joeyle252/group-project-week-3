f30d3d364582b53ff51259d547a2b07ae6f40934
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-12 15:33:12 +0700

    basic html for group

---
+++ main.js
@@ -0,0 +1,11 @@
+// let textArea = document.getElementById ("contentBox");
+
+// let countLetter = () => {
+//    let remain = 140 - textArea.value.length;
+   
+//    document.getElementById('charCountArea").innerHTML =  `${remain}chars`
+// }
+
+// textArea.addEventListener('input',countLetter);
+
+
